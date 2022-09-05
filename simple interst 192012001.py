e3cf2bec5bbf3c521a3d9470b3b49f423b13f87e
Manju9703 <112850860+Manju9703@users.noreply.github.com>
2022-09-05 14:29:54 +0530

    Update simple interst 192012001.py

--- simple interst 192012001.py
+++ simple interst 192012001.py
@@ -1,5 +1,5 @@
 
-p=float(input('Enter the value of initial pricipal'))
+p=float(input('Enter the value of initial priciple'))
 r=float(input('enter the intresrt rate'))
 t=float(input('enter the time'))
 A=(p*r*t)/100
