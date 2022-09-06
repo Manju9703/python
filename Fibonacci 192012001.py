15594ad0e946644621cf96a78b17cff2274116a3
Manju9703 <112850860+Manju9703@users.noreply.github.com>
2022-09-06 13:57:16 +0530

    Add files via upload

---
+++ Fibonacci 192012001.py
@@ -0,0 +1,8 @@
+def recur_fibo(n):
+   if n <= 1:
+       return n
+   else:
+       return(recur_fibo(n-1) + recur_fibo(n-2))
+n =int(input("enter the number "))
+for i in range(n):
+       print(recur_fibo(i))
