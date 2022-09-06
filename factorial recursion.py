8a970b9c0305b6ffafd51631b3b9647d17abf145
Manju9703 <112850860+Manju9703@users.noreply.github.com>
2022-09-06 16:40:55 +0530

    Add files via upload

---
+++ factorial recursion.py
@@ -0,0 +1,12 @@
+def recur_factorial(n):
+   if n == 1:
+       return n
+   else:
+       return n*recur_factorial(n-1)
+n =int(input("enter the number"))
+if n < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif n == 0:
+   print("The factorial of 0 is 1")
+else:
+   print("The factorial of", n, "is", recur_factorial(n))
