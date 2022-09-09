d8a75bb24a4dca318fad571cb2554aedf0a25283
Manju9703 <112850860+Manju9703@users.noreply.github.com>
2022-09-09 09:07:08 +0530

    Add files via upload

---
+++ pyramid for 11 22.py
@@ -0,0 +1,9 @@
+n=int(input("enter the no.of rows : "))
+k=2*n-2
+for i in range (0,n):
+    for j in range (1,k):
+          print(end='')
+    k=k-2
+    for j in range (1,(i)+2):
+          print(j,end='')
+    print('\r')
